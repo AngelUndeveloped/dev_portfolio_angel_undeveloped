50e02a55037b851c6ec99e20aff25c6a14c7199a
AngelUndeveloped <angelyairmendezsandoval@gmail.com>
2024-07-22 00:57:06 -0600

    added env file, and added supabase credentials

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
+import os
 from flask import Flask, render_template, url_for
 import json
 import supabase
 import dotenv
 
 # We need to access the env file for api keys
+dotenv_path = os.path.join(os.getcwd(), '.venv\\.env')
+# Load the .env file
+dotenv.load_dotenv(dotenv_path)
+
+# Access the API key
+SUPABASE_URL = os.environ.get('SUPABASE_URL')
+SUPABASE_KEY = os.environ.get('SUPABASE_KEY')
+
+print("Checking what your current credentials are:")
+print("SUPABASE_URL:", SUPABASE_URL)
+print("SUPABASE_KEY:", SUPABASE_KEY)
 
 # Create some random data for this projects
 table_data = [
