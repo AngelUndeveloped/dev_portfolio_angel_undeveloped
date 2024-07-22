1030a1225d07ecb746c2de3e6b8adb516642b914
AngelUndeveloped <angelyairmendezsandoval@gmail.com>
2024-07-22 00:17:36 -0600

    added a manual data table to test how to display a table

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template, url_for
+import json
+
+# Create some random data for this projects
+table_data = [
+    {"name": "John", "age": 30, "country": "USA"},
+    {"name": "Jane", "age": 25, "country": "Canada"},
+    {"name": "Bob", "age": 40, "country": "Mexico"}
+]
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', data=table_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+    
